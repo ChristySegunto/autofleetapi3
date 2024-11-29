4e779bba93aec1ca35e1eeb77b1dbb58a97598a4
ChristySegunto <christysgnt@gmail.com>
2024-11-29 22:48:46 +0800

    changes

--- obj/Debug/net8.0/autofleetapi3.AssemblyInfo.cs
+++ obj/Debug/net8.0/autofleetapi3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("autofleetapi3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b93818ba4c383f774c889c3397be2a73433af4d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("autofleetapi3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("autofleetapi3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
